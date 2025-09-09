49b55f6405adbdd41497250781bfc58ddbd58042
yani <yanazhuchok@gmail.com>
2025-09-09 17:34:16 +0300

    impl: kafka connect

--- user-service/src/main/java/com/dulfinne/randomgame/userservice/kafka/config/PaymentDeserializer.java
+++ user-service/src/main/java/com/dulfinne/randomgame/userservice/kafka/config/PaymentDeserializer.java
@@ -3,6 +3,7 @@ package com.dulfinne.randomgame.userservice.kafka.config;
 import com.dulfinne.randomgame.userservice.exception.UnableParseMessageException;
 import com.dulfinne.randomgame.userservice.kafka.entity.Payment;
 import com.dulfinne.randomgame.userservice.util.ExceptionKeys;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.NoArgsConstructor;
 import org.apache.kafka.common.serialization.Deserializer;
@@ -17,7 +18,8 @@ public class PaymentDeserializer implements Deserializer<Payment> {
 
   @Override
   public void configure(Map<String, ?> configs, boolean isKey) {
-    objectMapper = new ObjectMapper();
+    objectMapper =
+        new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
   }
 
   @Override
