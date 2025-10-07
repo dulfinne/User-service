7d6b86ec68095ecfe06c76d019206ced64d36eb3
yani <yanazhuchok@gmail.com>
2025-10-07 10:58:50 +0300

    fix: rm embedded kafka and add kafka container

--- user-service/src/test/java/com/dulfinne/randomgame/userservice/integration/IntegrationTestBase.java
+++ user-service/src/test/java/com/dulfinne/randomgame/userservice/integration/IntegrationTestBase.java
@@ -21,7 +21,8 @@ import static io.restassured.RestAssured.given;
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 public abstract class IntegrationTestBase {
 
-  @LocalServerPort  private int port;
+  @LocalServerPort
+  private int port;
 
   protected RequestSpecification withAuth(String username) {
     return given()
